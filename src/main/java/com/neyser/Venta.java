a2f97ca3757dea6398faa82a4367b49f0629e67e
neyser <neyser@digitechfp.com>
2023-10-02 11:13:23 +0200

    Cambios

--- src/main/java/com/neyser/Venta.java
+++ src/main/java/com/neyser/Venta.java
@@ -1,9 +1,7 @@
 package com.neyser;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Venta {
@@ -20,6 +18,7 @@ public class Venta {
         System.out.println(
                 "| 1. Registrar un producto\t\t|\n" +
                 "| 2. Registrar una Venta\t\t|\n" +
+                        "| 3. Mostrar productos\t\t\t|\n" +
                 "| 0. Cerrar Programa\t\t\t|");
         System.out.println("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
 
@@ -33,7 +32,11 @@ public class Venta {
             case "2":
                 registrarVenta();
                 break;
+            case "3":
+                mostrarProductos();
+                break;
             case "0":
+                sc1.close();
                 System.exit(0);
             default:
                 System.out.println("Debe seleccionar una opción válida");
@@ -139,6 +142,221 @@ public class Venta {
         menu();
 
 
+    }
+
+    public void mostrarProductos() throws IOException {
+        File file2 = new File("Productos");
+        String[] pathnames;
+        pathnames = file2.list();
+
+        System.out.println("______________________________");
+        System.out.println("           PRODUCTOS         ");
+        System.out.println("______________________________");
+
+        System.out.println("Nombre\t\tPrecio\tCantidad\t");
+
+        for (String pathname: pathnames) {
+            File file1 = new File(pathname);
+
+            String prod = file1.getName().substring(0,file1.getName().length()-4);
+            System.out.println(leerProducto(prod));
+        }
+
+        System.out.println("______________________________");
+
+        System.out.println("Ingresar el producto a comprar:");
+        String productoCompra = sc1.nextLine();
+
+        System.out.println("Ingresar la cantidad a comprar:");
+        Integer cantCompra = Integer.parseInt(sc1.nextLine());
+
+        if (obtenerCantidadProducto(productoCompra)-cantCompra<0)
+        {
+            System.out.println("La cantidad debe ser menor a "+cantCompra);
+        } else
+        {
+            actualizarProducto(productoCompra, 15.02,cantCompra);
+        }
+
+
+
+
+        System.out.println("Cantidad de "+ productoCompra +" = " + obtenerCantidadProducto(productoCompra));
+
+    }
+
+    public String leerProducto(String producto) throws IOException {
+
+        File file1 = new File("Productos/"+producto+".txt");
+        file1.createNewFile();
+
+        String resultado = "";
+
+        try
+        {
+            FileReader fileReader1 = new FileReader(file1);
+            BufferedReader bufferedReader1 = new BufferedReader(fileReader1);
+            String strCurrentLine;
+
+            while ((strCurrentLine=bufferedReader1.readLine()) != null)
+            {
+                String[] parts = strCurrentLine.split("\\|");
+
+                if (Integer.parseInt(parts[2])!=0)
+                {
+                    resultado+=parts[0];
+                    resultado+="\t\t";
+                    resultado+=parts[1];
+                    resultado+="\t";
+                    resultado+=parts[2];
+                }
+
+            }
+
+            if (fileReader1 != null)
+            {
+                fileReader1.close();
+            }
+
+        } catch (FileNotFoundException e1)
+        {
+            e1.printStackTrace();
+        } catch (IOException e2)
+        {
+            e2.printStackTrace();
+        }
+
+        return resultado;
+
+    }
+    public void comprar(String producto)
+    {
+        System.out.println();
+    }
+    public Integer obtenerCantidadProducto(String producto)
+    {
+        Integer resultado = 0;
+
+        try
+        {
+            File file1 = new File("Productos/"+producto+".txt");
+            FileReader fileReader1 = new FileReader(file1);
+
+            BufferedReader bufferedReader1 = new BufferedReader(fileReader1);
+
+            String strCurrentLine;
+
+            while ((strCurrentLine=bufferedReader1.readLine()) != null)
+            {
+                //System.out.println(strCurrentLine);
+
+                String[] parts = strCurrentLine.split("\\|");
+
+                if (Integer.parseInt(parts[2])!=0)
+                {
+                    resultado=Integer.parseInt(parts[2]);
+                }
+
+
+            }
+
+            if (fileReader1 != null)
+            {
+                fileReader1.close();
+            }
+
+        } catch (FileNotFoundException e1)
+        {
+            e1.printStackTrace();
+        } catch (IOException e2)
+        {
+            e2.printStackTrace();
+        }
+
+        return resultado;
+
+    }
+    public Integer obtenerPrecioProducto(String producto)
+    {
+        Double resultado = 0.0;
+
+        try
+        {
+            File file1 = new File("Productos/"+producto+".txt");
+            FileReader fileReader1 = new FileReader(file1);
+
+            BufferedReader bufferedReader1 = new BufferedReader(fileReader1);
+
+            String strCurrentLine;
+
+            while ((strCurrentLine=bufferedReader1.readLine()) != null)
+            {
+                //System.out.println(strCurrentLine);
+
+                String[] parts = strCurrentLine.split("\\|");
+
+                if (Integer.parseInt(parts[2])!=0)
+                {
+                    resultado=Double.parseDouble(parts[1]);
+                }
+
+
+            }
+
+            if (fileReader1 != null)
+            {
+                fileReader1.close();
+            }
+
+        } catch (FileNotFoundException e1)
+        {
+            e1.printStackTrace();
+        } catch (IOException e2)
+        {
+            e2.printStackTrace();
+        }
+
+        return resultado;
+
+    }
+
+    public void actualizarProducto(String prod, Double precio, Integer cantidad) throws IOException {
+        Producto producto = new Producto();
+        producto.setDenominacion(prod);
+        producto.setPrecio(precio);
+        producto.setDisponibilidad(cantidad);
+
+        File file1 = new File("Productos/"+prod+".txt");
+
+        try
+        {
+            if (file1.createNewFile())
+            {
+                System.out.println("El fichero ha sido creado");
+            } else
+            {
+                System.out.println("El fichero NO ha sido creado");
+            }
+        } catch (IOException e)
+        {
+            System.out.println("Error, no se pudo crear el fichero "+e.getMessage());
+            e.printStackTrace();
+        }
+
+        // Escribir en fichero
+        FileWriter fileWriter1 = new FileWriter(file1, true);
+        BufferedWriter bufferedWriter1 = new BufferedWriter(fileWriter1);
+
+        bufferedWriter1.write(producto.getDenominacion()+"|");
+        bufferedWriter1.write(producto.getPrecio()+"|");
+        bufferedWriter1.write(producto.getDisponibilidad().toString());
+
+        bufferedWriter1.close();
+        System.out.println("Se ha escrito en el fichero");
+
+        //sc1.close();
+
+        menu();
     }
 
 }
